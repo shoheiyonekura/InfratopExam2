5280146961454f5498983d4d95364b61f93c6cf7
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-04-24 09:03:01 +0000

    first

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def index
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def show
+    
+  end
+  
+end
