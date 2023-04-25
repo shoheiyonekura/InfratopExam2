5da609cc955896d217811e969141c1addceb530c
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-04-25 10:08:50 +0000

    indexにshow,edit,destroyを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,6 +29,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   private
   #ストロングパラメータ
   def book_params
