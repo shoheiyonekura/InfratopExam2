31c5cb44ad17a1bc76c537f941e8551db96cf2c5
shoheiyonekura <38638474+shoheiyonekura@users.noreply.github.com>
2023-04-26 06:03:27 +0000

    RSpecでのテストを実行できるように

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save 
-      flash[:notice] = "New Book Create Successfully."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
+  root :to => 'homes#top'
   #resoucesでのルーティングでupdate
   patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
